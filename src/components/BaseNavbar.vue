fd08c87ccc9824a316f687b900ce5cc3b73dae6a
raflihaidar <rafli.haidar30@gmail.com>
2024-12-13 10:09:19 +0700

    fix routing

--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -79,4 +79,35 @@ const navigateWithAnimation = (event) => {
       <BaseCalendar :isOpen="calenderOpen" @close="calenderOpen = false" />
     </nav>
   </Teleport>
+
+  <Teleport to="body">
+    <section
+      v-if="isMenuOpen"
+      class="fixed flex flex-col justify-between w-screen h-[90%] p-5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark-default z-50"
+    >
+      <nav class="w-full relative">
+        <RouterLink to="/">
+          <figure class="w-auto absolute cursor-pointer left-1/2 transform -translate-x-1/2">
+            <img src="../assets/images/Logo.svg" alt="Logo" class="w-full h-full" />
+          </figure>
+        </RouterLink>
+        <CloseIcon @click="closeMenu" class="ml-auto cursor-pointer" />
+      </nav>
+
+      <ul class="w-full grid gap-y-3 text-center text-light-default text-3xl font-medium">
+        <li>
+          <RouterLink to="/accommodation">Accommodation</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/experiences">Experiences</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/dining">Dining</RouterLink>
+        </li>
+      </ul>
+      <footer class="w-full font-medium text-xl text-center text-light-default">
+        <h3>BOOK YOUR STAY</h3>
+      </footer>
+    </section>
+  </Teleport>
 </template>
